7a3d71a3a265a36d3fa1226afba82b58b7c2642d
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-11-27 19:30:03 +0800

    finished

--- VTK_Project/main.cpp
+++ VTK_Project/main.cpp
@@ -8,19 +8,55 @@
 
 **********************************************************************/
 
-#include <vtkRenderWindow.h>
 #include <vtkSmartPointer.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkCylinderSource.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkProperty.h>
 
 int main()
 {
+    vtkSmartPointer<vtkCylinderSource> cylinder =
+        vtkSmartPointer<vtkCylinderSource>::New();
+    cylinder->SetHeight( 3.0 );
+    cylinder->SetRadius( 1.0 );
+    cylinder->SetResolution( 10 );
+
+    vtkSmartPointer<vtkPolyDataMapper> cylinderMapper =
+        vtkSmartPointer<vtkPolyDataMapper>::New();
+    cylinderMapper->SetInputConnection( cylinder->GetOutputPort() );
+
+    vtkSmartPointer<vtkActor> cylinderActor =
+        vtkSmartPointer<vtkActor>::New();
+    cylinderActor->SetMapper( cylinderMapper );
+    cylinderActor->GetProperty()->SetColor(1.0, 0.0, 0.0);
+
+    vtkSmartPointer<vtkRenderer> renderer =
+        vtkSmartPointer<vtkRenderer>::New();
+    renderer->AddActor( cylinderActor );
+    renderer->SetBackground( 1.0, 1.0, 1.0 );
+
     vtkSmartPointer<vtkRenderWindow> renWin =
         vtkSmartPointer<vtkRenderWindow>::New();
+    renWin->AddRenderer( renderer );
+    renWin->SetSize( 640, 480 );
     renWin->Render();
+    renWin->SetWindowName("RenderCylinder");
 
-    renWin->SetWindowName("TestVTKInstall");
-    renWin->SetSize(640, 480);
-    renWin->Render();
+    vtkSmartPointer<vtkRenderWindowInteractor> iren =
+        vtkSmartPointer<vtkRenderWindowInteractor>::New();
+    iren->SetRenderWindow(renWin);
 
-    std::cin.get();
+    vtkSmartPointer<vtkInteractorStyleTrackballCamera> style =
+        vtkSmartPointer<vtkInteractorStyleTrackballCamera>::New();
+    iren->SetInteractorStyle(style);
+
+    iren->Initialize();
+    iren->Start();
+
     return EXIT_SUCCESS;
 }

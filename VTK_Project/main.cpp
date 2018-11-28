64571cc57f4595669d62c1f0a9df9ff364105ec0
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-11-28 19:56:45 +0800

    finished pipeline execute

--- VTK_Project/main.cpp
+++ VTK_Project/main.cpp
@@ -14,51 +14,67 @@
 #include <vtkActor.h>
 #include <vtkRenderWindowInteractor.h>
 
-#include <vtkMarchingCubes.h>
-#include <vtkPolyDataMapper.h>
-#include <vtkStructuredPointsReader.h>
+#include <vtkBMPReader.h>
+#include <vtkImageData.h>
+#include <vtkImageViewer2.h>
 
 
 int main(int argc, char* argv[])
 {
     if (argc < 2)
     {
-        std::cout<<argv[0]<<" "<<"VTK-File(*.vtk)"<<std::endl;
+        std::cout<<argv[0]<<" "<<"BMP-File(*.bmp)"<<std::endl;
         return EXIT_FAILURE;
     }
 
-    vtkSmartPointer<vtkStructuredPointsReader> reader =
-            vtkSmartPointer<vtkStructuredPointsReader>::New();
+    vtkSmartPointer<vtkBMPReader> reader =
+            vtkSmartPointer<vtkBMPReader>::New();
+    std::cout<<"Modification Time of reader (After New()): "
+            <<reader->GetMTime()<<std::endl;
     reader->SetFileName(argv[1]);
+    std::cout<<"Modification Time of reader (After SetFileName()): "
+            <<reader->GetMTime()<<std::endl;
 
-    vtkSmartPointer<vtkMarchingCubes> marchingCubes =
-            vtkSmartPointer<vtkMarchingCubes>::New();
-    marchingCubes->SetInputConnection(reader->GetOutputPort());
-    marchingCubes->SetValue(0,500);
+    vtkImageData *imageData = reader->GetOutput();
+    std::cout<<"Modification Time of reader (After GetOutput()): "
+            <<reader->GetMTime()<<std::endl;
+    reader->Update();
+    std::cout<<"Modification Time of reader (After Update()): "
+            <<reader->GetMTime()<<std::endl;
 
-    vtkSmartPointer<vtkPolyDataMapper> mapper =
-            vtkSmartPointer<vtkPolyDataMapper>::New();
-    mapper->SetInputConnection(marchingCubes->GetOutputPort());
+    int extent[6];
+    imageData->GetExtent(extent);
+    std::cout<<"Extent of image: "<<extent[0]<<" "
+        <<extent[1]<<" "<<extent[2]<<" "<<extent[3]<<" "
+        <<extent[4]<<" "<<extent[5]<<" "<<std::endl;
+
+    vtkSmartPointer<vtkImageViewer2> viewer =
+            vtkSmartPointer<vtkImageViewer2>::New();
 
     //////////////////////////////////////渲染引擎部分////////////////////////////////////
-    vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
-    actor->SetMapper(mapper);
+//    vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
+//    actor->SetMapper(mapper);
 
-    vtkSmartPointer<vtkRenderer> renderer =
-        vtkSmartPointer<vtkRenderer>::New();
-    renderer->AddActor(actor);
-    renderer->SetBackground(1.0, 1.0, 1.0);
+//    vtkSmartPointer<vtkRenderer> renderer =
+//        vtkSmartPointer<vtkRenderer>::New();
+//    renderer->AddActor(actor);
+//    renderer->SetBackground(1.0, 1.0, 1.0);
 
-    vtkSmartPointer<vtkRenderWindow> renWin =
-        vtkSmartPointer<vtkRenderWindow>::New();
-    renWin->AddRenderer(renderer);
-    renWin->SetSize( 800, 600 );
-    renWin->Render();
-    renWin->SetWindowName("vtkPipelineDemo");
+//    vtkSmartPointer<vtkRenderWindow> renWin =
+//        vtkSmartPointer<vtkRenderWindow>::New();
+//    renWin->AddRenderer(renderer);
+//    renWin->SetSize( 800, 600 );
+//    renWin->Render();
+//    renWin->SetWindowName("vtkPipelineDemo");
 
     vtkSmartPointer<vtkRenderWindowInteractor> interactor =
         vtkSmartPointer<vtkRenderWindowInteractor>::New();
-    interactor->SetRenderWindow(renWin);
+    viewer->SetupInteractor(interactor);
+    viewer->SetInputData(imageData);
+    viewer->Render();
+
+    viewer->SetSize(800,600);
+    viewer->GetRenderWindow()->SetWindowName("vtkPipelineExecute");
 
     interactor->Initialize();
     interactor->Start();

d508100dac139cf080877861b67bea9c89088741
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-11-29 11:24:16 +0800

    ok

--- VTK_Project/main.cpp
+++ VTK_Project/main.cpp
@@ -9,63 +9,26 @@
 **********************************************************************/
 
 #include <vtkSmartPointer.h>
-#include <vtkRenderWindow.h>
-#include <vtkRenderer.h>
-#include <vtkRenderWindowInteractor.h>
-#include <vtkInteractorStyleTrackballCamera.h>
-#include <vtkPolyDataMapper.h>
-#include <vtkPlaneSource.h>
-#include <vtkActor.h>
-#include <vtkProperty.h>
-#include <vtkJPEGReader.h>
-#include <vtkTexture.h>
+#include <vtkPoints.h>
+#include <vtkPolyData.h>
+#include <vtkPolyDataWriter.h>
 
 int main(int argc, char* argv[])
 {
-    if(argc < 2)
-    {
-        std::cout<<argv[0]<<" "<<"TextureFile(*.jpg)"<<std::endl;
-        return EXIT_FAILURE;
-    }
+    vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
+    points->InsertNextPoint(1.0,0.0,0.0);
+    points->InsertNextPoint(0.0,0.0,0.0);
+    points->InsertNextPoint(0.0,1.0,0.0);
 
-    vtkSmartPointer<vtkJPEGReader> reader =
-            vtkSmartPointer<vtkJPEGReader>::New();
-    reader->SetFileName(argv[1]);
+    vtkSmartPointer<vtkPolyData> polydata =
+            vtkSmartPointer<vtkPolyData>::New();
+    polydata->SetPoints(points);
 
-    vtkSmartPointer<vtkTexture> texture =
-            vtkSmartPointer<vtkTexture>::New();
-    texture->SetInputConnection(reader->GetOutputPort());
-    texture->InterpolateOn();
-
-    vtkSmartPointer<vtkPlaneSource> plane =
-            vtkSmartPointer<vtkPlaneSource>::New();
-    vtkSmartPointer<vtkPolyDataMapper> mapper =
-            vtkSmartPointer<vtkPolyDataMapper>::New();
-    mapper->SetInputConnection(plane->GetOutputPort());
-
-    vtkSmartPointer<vtkActor> actor =
-            vtkSmartPointer<vtkActor>::New();
-    actor->SetMapper(mapper);
-    actor->SetTexture(texture);
-
-    vtkSmartPointer<vtkRenderer> renderer =
-        vtkSmartPointer<vtkRenderer>::New();
-    renderer->AddActor( actor );
-    renderer->SetBackground( 1.0, 1.0, 1.0 );
-
-    vtkSmartPointer<vtkRenderWindow> renWin =
-        vtkSmartPointer<vtkRenderWindow>::New();
-    renWin->AddRenderer( renderer );
-    renWin->SetSize( 640, 480 );
-    renWin->Render();
-    renWin->SetWindowName("TextureExample");
-
-    vtkSmartPointer<vtkRenderWindowInteractor> iren =
-        vtkSmartPointer<vtkRenderWindowInteractor>::New();
-    iren->SetRenderWindow(renWin);
-
-    iren->Initialize();
-    iren->Start();
+    vtkSmartPointer<vtkPolyDataWriter> writer =
+            vtkSmartPointer<vtkPolyDataWriter>::New();
+    writer->SetFileName("triangle.vtk");
+    writer->SetInputData(polydata);
+    writer->Write();
 
     return EXIT_SUCCESS;
 }

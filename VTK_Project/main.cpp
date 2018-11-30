dc28e889d5bd80d9abd57558a5b07a3e502c0daa
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-11-30 09:52:24 +0800

    finished

--- VTK_Project/main.cpp
+++ VTK_Project/main.cpp
@@ -13,11 +13,14 @@
 #include <vtkRenderer.h>
 #include <vtkRenderWindowInteractor.h>
 
-#include <vtkConeSource.h>
 #include <vtkPolyDataMapper.h>
 #include <vtkPolyData.h>
 #include <vtkActor.h>
 #include <iostream>
+#include <vtkPolygon.h>
+#include <vtkTriangle.h>
+#include <vtkProperty.h>
+#include <vtkCellArray.h>
 
 int main(int argc, char* argv[])
 {
@@ -27,20 +30,41 @@ int main(int argc, char* argv[])
 //        return EXIT_FAILURE;
 //    }
 
-    vtkSmartPointer<vtkConeSource> coneSrc =
-            vtkSmartPointer<vtkConeSource>::New();
-    coneSrc->Update();
+    vtkSmartPointer<vtkPoints> points =
+            vtkSmartPointer<vtkPoints>::New();
+    points->InsertNextPoint(0.0,0.0,0.0);
+    points->InsertNextPoint(1.0,0.0,0.0);
+    points->InsertNextPoint(1.0,1.0,0.0);
+    points->InsertNextPoint(0.0,1.0,0.0);
+    points->InsertNextPoint(2.0,0.0,0.0);
 
-    vtkSmartPointer<vtkPolyData> cone = coneSrc->GetOutput();
-    int nPoints = cone->GetNumberOfPoints();
-    int nCells = cone->GetNumberOfCells();
+    vtkSmartPointer<vtkPolygon> polygon =
+            vtkSmartPointer<vtkPolygon>::New();
+    polygon->GetPointIds()->SetNumberOfIds(4);
+    polygon->GetPointIds()->SetId(0,0);
+    polygon->GetPointIds()->SetId(1,1);
+    polygon->GetPointIds()->SetId(2,2);
+    polygon->GetPointIds()->SetId(3,3);
 
-    std::cout<<"Points number:"<<nPoints<<std::endl;
-    std::cout<<"Cells  number:"<<nCells<<std::endl;
+    vtkSmartPointer<vtkTriangle> triangle =
+            vtkSmartPointer<vtkTriangle>::New();
+    triangle->GetPointIds()->SetId(0,1);
+    triangle->GetPointIds()->SetId(1,2);
+    triangle->GetPointIds()->SetId(2,4);
+
+    vtkSmartPointer<vtkCellArray> cells =
+            vtkSmartPointer<vtkCellArray>::New();
+    cells->InsertNextCell(polygon);
+    cells->InsertNextCell(triangle);
+
+    vtkSmartPointer<vtkPolyData> polygonPolyData =
+            vtkSmartPointer<vtkPolyData>::New();
+    polygonPolyData->SetPoints(points);
+    polygonPolyData->SetPolys(cells);
 
     vtkSmartPointer<vtkPolyDataMapper> mapper =
             vtkSmartPointer<vtkPolyDataMapper>::New();
-    mapper->SetInputData(cone);
+    mapper->SetInputData(polygonPolyData);
 
     vtkSmartPointer<vtkActor> actor =
             vtkSmartPointer<vtkActor>::New();
@@ -49,7 +73,7 @@ int main(int argc, char* argv[])
     vtkSmartPointer<vtkRenderer> renderer =
             vtkSmartPointer<vtkRenderer>::New();
     renderer->AddActor(actor);
-    renderer->SetBackground(1.0,1.0,1.0);
+    renderer->SetBackground(0.5,0.5,0.5);
 
     // Setup render window
     vtkSmartPointer<vtkRenderWindow> renderWindow =
@@ -57,7 +81,7 @@ int main(int argc, char* argv[])
     renderWindow->AddRenderer(renderer);
     renderWindow->SetSize( 800, 600 );
     renderWindow->Render();
-    renderWindow->SetWindowName("PolyDataSource");
+    renderWindow->SetWindowName("PolyDataNew");
 
     // Setup render window interactor
     vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =

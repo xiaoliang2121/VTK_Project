6c4a72aee9032d390822549de9b5597e1fa6e64f
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-11-30 11:39:03 +0800

    ok

--- VTK_Project/main.cpp
+++ VTK_Project/main.cpp
@@ -13,10 +13,15 @@
 #include <vtkRenderer.h>
 #include <vtkRenderWindowInteractor.h>
 
-#include <vtkImageData.h>
-#include <vtkImageCanvasSource2D.h>
-#include <vtkInteractorStyleImage.h>
-#include <vtkImageActor.h>
+#include <vtkPointData.h>
+#include <vtkActor.h>
+#include <vtkScalarBarActor.h>
+#include <vtkPolyData.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkCubeSource.h>
+#include <vtkProperty.h>
+#include <vtkTriangleFilter.h>
+#include <vtkMassProperties.h>
 
 int main(int argc, char* argv[])
 {
@@ -26,44 +31,56 @@ int main(int argc, char* argv[])
 //        return EXIT_FAILURE;
 //    }
 
-    vtkSmartPointer<vtkImageCanvasSource2D> canvas =
-            vtkSmartPointer<vtkImageCanvasSource2D>::New();
-    canvas->SetScalarTypeToUnsignedChar();
-    canvas->SetNumberOfScalarComponents(1);
-    canvas->SetExtent(0,100,0,100,0,0);
-    canvas->SetDrawColor(0,0,0,0);
-    canvas->FillBox(0,100,0,100);
-    canvas->SetDrawColor(255,0,0,0);
-    canvas->FillBox(20,40,20,40);
-    canvas->Update();
+    vtkSmartPointer<vtkCubeSource> cubeSource =
+            vtkSmartPointer<vtkCubeSource>::New();
+    cubeSource->Update();
 
-    vtkSmartPointer<vtkImageActor> redActor =
-            vtkSmartPointer<vtkImageActor>::New();
-    redActor->SetInputData(canvas->GetOutput());
+    vtkSmartPointer<vtkTriangleFilter> triFilter =
+            vtkSmartPointer<vtkTriangleFilter>::New();
+    triFilter->SetInputData(cubeSource->GetOutput());
+    triFilter->Update();
 
-    double redViewport[4] = {0.0, 0.0, 1.0, 1.0};
-    vtkSmartPointer<vtkRenderer> redRenderer =
+    vtkSmartPointer<vtkMassProperties> massProp =
+            vtkSmartPointer<vtkMassProperties>::New();
+    massProp->SetInputData(triFilter->GetOutput());
+    float vol = massProp->GetVolume();
+    float area = massProp->GetSurfaceArea();
+    float maxArea = massProp->GetMaxCellArea();
+    float minArea = massProp->GetMinCellArea();
+
+    std::cout<<"Volume      :"<<vol<<std::endl;
+    std::cout<<"Surface Area:"<<area<<std::endl;
+    std::cout<<"Max Area    :"<<maxArea<<std::endl;
+    std::cout<<"Min Area    :"<<minArea<<std::endl;
+
+    vtkSmartPointer<vtkPolyDataMapper> mapper =
+            vtkSmartPointer<vtkPolyDataMapper>::New();
+//    mapper->SetInputData(triFilter->GetOutput());
+    mapper->SetInputConnection(cubeSource->GetOutputPort());
+
+    vtkSmartPointer<vtkActor> actor =
+            vtkSmartPointer<vtkActor>::New();
+    actor->SetMapper(mapper);
+    actor->GetProperty()->SetColor(0,1,0);
+    actor->GetProperty()->SetEdgeColor(1,0,0);
+    actor->GetProperty()->SetEdgeVisibility(1);
+
+    vtkSmartPointer<vtkRenderer> renderer =
             vtkSmartPointer<vtkRenderer>::New();
-    redRenderer->SetViewport(redViewport);
-    redRenderer->AddActor(redActor);
-    redRenderer->ResetCamera();
-    redRenderer->SetBackground(1.0,1.0,1.0);
+    renderer->AddActor(actor);
+    renderer->SetBackground(1.0,1.0,1.0);
 
     // Setup render window
     vtkSmartPointer<vtkRenderWindow> renderWindow =
         vtkSmartPointer<vtkRenderWindow>::New();
-    renderWindow->AddRenderer(redRenderer);
+    renderWindow->AddRenderer(renderer);
     renderWindow->SetSize( 800, 600 );
     renderWindow->Render();
-    renderWindow->SetWindowName("ImageCanvasSource2D");
+    renderWindow->SetWindowName("PolyDataMassProperty");
 
     // Setup render window interactor
     vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
         vtkSmartPointer<vtkRenderWindowInteractor>::New();
-    vtkSmartPointer<vtkInteractorStyleImage> style =
-        vtkSmartPointer<vtkInteractorStyleImage>::New();
-
-    renderWindowInteractor->SetInteractorStyle(style);
     // Render and start interaction
     renderWindowInteractor->SetRenderWindow(renderWindow);
     renderWindowInteractor->Initialize();

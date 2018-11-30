39347145649c5de33143d9d5113591e3b6cb1fe6
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-11-30 15:59:59 +0800

    no change

--- VTK_Project/main.cpp
+++ VTK_Project/main.cpp
@@ -1,45 +1,51 @@
 ﻿/**********************************************************************
 
-  文件名: 2.1_RenderCylinder.cpp
+  文件名: 6.2_PolyDataNormal.cpp
   Copyright (c) 张晓东, 罗火灵. All rights reserved.
-  更多信息请访问: 
+  更多信息请访问:
     http://www.vtkchina.org (VTK中国)
-	http://blog.csdn.net/www_doling_net (东灵工作室) 
+    http://blog.csdn.net/www_doling_net (东灵工作室)
 
 **********************************************************************/
 
 #include <vtkSmartPointer.h>
-#include <vtkRenderWindow.h>
-#include <vtkRenderer.h>
-#include <vtkRenderWindowInteractor.h>
-
-#include <vtkPolyData.h>
-#include <vtkPolyDataMapper.h>
 #include <vtkActor.h>
-
+#include <vtkPolyDataMapper.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkXMLPolyDataReader.h>
 #include <vtkPolyDataReader.h>
-#include <vtkPolyDataNormals.h>
 #include <vtkPLYReader.h>
-#include <vtkMaskPoints.h>
-#include <vtkArrowSource.h>
+#include <vtkFloatArray.h>
+#include <vtkPointData.h>
+#include <vtkPolyDataNormals.h>
 #include <vtkGlyph3D.h>
+#include <vtkArrowSource.h>
+#include <vtkSphereSource.h>
+#include <vtkMaskPoints.h>
 #include <vtkProperty.h>
 
-int main(int argc, char* argv[])
+//测试文件：../data/fran_cut.vtk
+int main(int argc, char *argv[])
 {
     if(argc < 2)
     {
-        std::cout<<argv[0]<<" "<<"*.vtk"<<std::endl;
+        std::cout<<argv[0]<<" *.vtk"<<std::endl;
         return EXIT_FAILURE;
     }
 
-    vtkSmartPointer<vtkPLYReader> reader =
-            vtkSmartPointer<vtkPLYReader>::New();
+//    vtkSmartPointer<vtkPLYReader> reader =
+//        vtkSmartPointer<vtkPLYReader>::New();
+//    reader->SetFileName(argv[1]);
+//    reader->Update();
+    vtkSmartPointer<vtkPolyDataReader> reader =
+        vtkSmartPointer<vtkPolyDataReader>::New();
     reader->SetFileName(argv[1]);
     reader->Update();
 
     vtkSmartPointer<vtkPolyDataNormals> normFilter =
-            vtkSmartPointer<vtkPolyDataNormals>::New();
+        vtkSmartPointer<vtkPolyDataNormals>::New();
     normFilter->SetInputData(reader->GetOutput());
     normFilter->SetComputePointNormals(1);
     normFilter->SetComputeCellNormals(0);
@@ -48,85 +54,84 @@ int main(int argc, char* argv[])
     normFilter->Update();
 
     vtkSmartPointer<vtkMaskPoints> mask =
-            vtkSmartPointer<vtkMaskPoints>::New();
+        vtkSmartPointer<vtkMaskPoints>::New();
     mask->SetInputData(normFilter->GetOutput());
     mask->SetMaximumNumberOfPoints(300);
     mask->RandomModeOn();
 
     vtkSmartPointer<vtkArrowSource> arrow =
-            vtkSmartPointer<vtkArrowSource>::New();
+        vtkSmartPointer<vtkArrowSource>::New();
+
     vtkSmartPointer<vtkGlyph3D> glyph =
-            vtkSmartPointer<vtkGlyph3D>::New();
+        vtkSmartPointer<vtkGlyph3D>::New();
     glyph->SetInputData(mask->GetOutput());
     glyph->SetSourceData(arrow->GetOutput());
     glyph->SetVectorModeToUseNormal();
     glyph->SetScaleFactor(0.01);
 
     vtkSmartPointer<vtkPolyDataMapper> originMapper =
-            vtkSmartPointer<vtkPolyDataMapper>::New();
+        vtkSmartPointer<vtkPolyDataMapper>::New();
     originMapper->SetInputData(reader->GetOutput());
 
     vtkSmartPointer<vtkActor> originActor =
-            vtkSmartPointer<vtkActor>::New();
+        vtkSmartPointer<vtkActor>::New();
     originActor->SetMapper(originMapper);
 
     vtkSmartPointer<vtkPolyDataMapper> normedMapper =
-            vtkSmartPointer<vtkPolyDataMapper>::New();
+        vtkSmartPointer<vtkPolyDataMapper>::New();
     normedMapper->SetInputData(normFilter->GetOutput());
 
     vtkSmartPointer<vtkActor> normedActor =
-            vtkSmartPointer<vtkActor>::New();
+        vtkSmartPointer<vtkActor>::New();
     normedActor->SetMapper(normedMapper);
 
     vtkSmartPointer<vtkPolyDataMapper> glyphMapper =
-            vtkSmartPointer<vtkPolyDataMapper>::New();
+        vtkSmartPointer<vtkPolyDataMapper>::New();
     glyphMapper->SetInputData(glyph->GetOutput());
 
     vtkSmartPointer<vtkActor> glyphActor =
-            vtkSmartPointer<vtkActor>::New();
+        vtkSmartPointer<vtkActor>::New();
     glyphActor->SetMapper(glyphMapper);
-    glyphActor->GetProperty()->SetColor(1.0,0.0,0.0);
+    glyphActor->GetProperty()->SetColor(1., 0.,0.);
 
     double originalViewport[4] = {0.0, 0.0, 0.33, 1.0};
     double normViewport[4] = {0.33, 0.0, 0.66, 1.0};
     double glphViewport[4] = {0.66, 0.0, 1.0, 1.0};
 
-    vtkSmartPointer<vtkRenderer> originRen =
-            vtkSmartPointer<vtkRenderer>::New();
-    originRen->SetViewport(originalViewport);
-    originRen->AddActor(originActor);
-    originRen->ResetCamera();
-    originRen->SetBackground(1.0,1.0,1.0);
+    vtkSmartPointer<vtkRenderer> originalRenderer =
+        vtkSmartPointer<vtkRenderer>::New();
+    originalRenderer->SetViewport(originalViewport);
+    originalRenderer->AddActor(originActor);
+    originalRenderer->ResetCamera();
+    originalRenderer->SetBackground(1.0, 1.0, 1.0);
 
-    vtkSmartPointer<vtkRenderer> normedRen =
-            vtkSmartPointer<vtkRenderer>::New();
-    normedRen->SetViewport(normViewport);
-    normedRen->AddActor(normedActor);
-    normedRen->ResetCamera();
-    normedRen->SetBackground(1.0,1.0,1.0);
+    vtkSmartPointer<vtkRenderer> normedRenderer =
+        vtkSmartPointer<vtkRenderer>::New();
+    normedRenderer->SetViewport(normViewport);
+    normedRenderer->AddActor(normedActor);
+    normedRenderer->ResetCamera();
+    normedRenderer->SetBackground(1.0, 1.0, 1.0);
 
-    vtkSmartPointer<vtkRenderer> glyphRen =
-            vtkSmartPointer<vtkRenderer>::New();
-    glyphRen->SetViewport(glphViewport);
-    glyphRen->AddActor(glyphActor);
-    glyphRen->AddActor(normedActor);
-    glyphRen->ResetCamera();
-    glyphRen->SetBackground(1.0,1.0,1.0);
+    vtkSmartPointer<vtkRenderer> glyphRenderer =
+        vtkSmartPointer<vtkRenderer>::New();
+    glyphRenderer->SetViewport(glphViewport);
+    glyphRenderer->AddActor(glyphActor);
+    glyphRenderer->AddActor(normedActor);
+    glyphRenderer->ResetCamera();
+    glyphRenderer->SetBackground(1.0, 1.0, 1.0);
 
-//================================================================================//
     vtkSmartPointer<vtkRenderWindow> renderWindow =
         vtkSmartPointer<vtkRenderWindow>::New();
-    renderWindow->AddRenderer(originRen);
-    renderWindow->AddRenderer(normedRen);
-    renderWindow->AddRenderer(glyphRen);
-    renderWindow->SetSize( 640, 480 );
+    renderWindow->AddRenderer(originalRenderer);
+    renderWindow->AddRenderer(normedRenderer);
+    renderWindow->AddRenderer(glyphRenderer);
+    renderWindow->SetSize(640, 320);
     renderWindow->Render();
     renderWindow->SetWindowName("PolyDataNormal");
 
     vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
         vtkSmartPointer<vtkRenderWindowInteractor>::New();
     renderWindowInteractor->SetRenderWindow(renderWindow);
-
     renderWindowInteractor->Initialize();
     renderWindowInteractor->Start();
 

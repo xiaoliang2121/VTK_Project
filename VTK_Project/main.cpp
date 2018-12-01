701f94cd2856b232a1acb18db22c2ca0e1c44d66
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-12-01 13:10:28 +0800

    test ok

--- VTK_Project/main.cpp
+++ VTK_Project/main.cpp
@@ -1,6 +1,6 @@
 ﻿/**********************************************************************
 
-  文件名: 6.7_PolyDataDelaunay2D.cpp
+  文件名: 6.7_PolyDataConstrainedDelaunay2D.cpp
   Copyright (c) 张晓东, 罗火灵. All rights reserved.
   更多信息请访问:
     http://www.vtkchina.org (VTK中国)
@@ -10,6 +10,8 @@
 
 #include <vtkSmartPointer.h>
 #include <vtkProperty.h>
+#include <vtkPolygon.h>
+#include <vtkCellArray.h>
 #include <vtkPoints.h>
 #include <vtkPolyData.h>
 #include <vtkPointData.h>
@@ -24,9 +26,10 @@
 
 int main(int, char *[])
 {
-    unsigned int gridSize = 10;
     vtkSmartPointer<vtkPoints> points =
         vtkSmartPointer<vtkPoints>::New();
+
+    unsigned int gridSize = 10;
     for(unsigned int x = 0; x < gridSize; x++)
     {
         for(unsigned int y = 0; y < gridSize; y++)
@@ -39,9 +42,43 @@ int main(int, char *[])
         vtkSmartPointer<vtkPolyData>::New();
     polydata->SetPoints(points);
 
+    vtkSmartPointer<vtkPolygon> poly =
+        vtkSmartPointer<vtkPolygon>::New();
+    /*poly->GetPointIds()->InsertNextId(22);
+    poly->GetPointIds()->InsertNextId(23);
+    poly->GetPointIds()->InsertNextId(24);
+    poly->GetPointIds()->InsertNextId(25);
+    poly->GetPointIds()->InsertNextId(35);
+    poly->GetPointIds()->InsertNextId(45);
+    poly->GetPointIds()->InsertNextId(44);
+    poly->GetPointIds()->InsertNextId(43);
+    poly->GetPointIds()->InsertNextId(42);
+    poly->GetPointIds()->InsertNextId(32);*/
+
+    poly->GetPointIds()->InsertNextId(32);
+    poly->GetPointIds()->InsertNextId(42);
+    poly->GetPointIds()->InsertNextId(43);
+    poly->GetPointIds()->InsertNextId(44);
+    poly->GetPointIds()->InsertNextId(45);
+    poly->GetPointIds()->InsertNextId(35);
+    poly->GetPointIds()->InsertNextId(25);
+    poly->GetPointIds()->InsertNextId(24);
+    poly->GetPointIds()->InsertNextId(23);
+    poly->GetPointIds()->InsertNextId(22);
+
+    vtkSmartPointer<vtkCellArray> cell =
+        vtkSmartPointer<vtkCellArray>::New();
+    cell->InsertNextCell(poly);
+
+    vtkSmartPointer<vtkPolyData> boundary =
+        vtkSmartPointer<vtkPolyData>::New();
+    boundary->SetPoints(points);
+    boundary->SetPolys(cell);
+
     vtkSmartPointer<vtkDelaunay2D> delaunay =
         vtkSmartPointer<vtkDelaunay2D>::New();
     delaunay->SetInputData(polydata);
+    delaunay->SetSourceData(boundary);
     delaunay->Update();
 
     vtkSmartPointer<vtkVertexGlyphFilter> glyphFilter =
@@ -81,11 +118,10 @@ int main(int, char *[])
         vtkSmartPointer<vtkRenderWindowInteractor>::New();
     renderWindowInteractor->SetRenderWindow(renderWindow);
 
-    renderWindow->SetSize(640, 320);
+    renderWindow->SetSize(640, 480);
     renderWindow->Render();
-    renderWindow->SetWindowName("PolyDataDelaunay2D");
+    renderWindow->SetWindowName("PolyDataConstrainedDelaunay2D");
     renderWindow->Render();
-
     renderWindowInteractor->Start();
 
     return EXIT_SUCCESS;

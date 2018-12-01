42c24ae911f428d26269dbe8714f9424fa5da5a0
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-12-01 15:02:57 +0800

    test ok

--- VTK_Project/main.cpp
+++ VTK_Project/main.cpp
@@ -1,63 +1,90 @@
 ﻿/**********************************************************************
 
-  文件名: 2.1_RenderCylinder.cpp
+  文件名: 6.9_TextureMap.cpp
   Copyright (c) 张晓东, 罗火灵. All rights reserved.
-  更多信息请访问: 
+  更多信息请访问:
     http://www.vtkchina.org (VTK中国)
-	http://blog.csdn.net/www_doling_net (东灵工作室) 
+    http://blog.csdn.net/www_doling_net (东灵工作室)
 
 **********************************************************************/
 
 #include <vtkSmartPointer.h>
+#include <vtkRenderWindowInteractor.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderer.h>
-#include <vtkRenderWindowInteractor.h>
-
+#include <vtkActor.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkTransformTextureCoords.h>
+#include <vtkTexture.h>
+#include <vtkTextureMapToSphere.h>
+#include <vtkTextureMapToCylinder.h>
 #include <vtkBMPReader.h>
-#include <vtkInteractorStyleImage.h>
-#include <vtkImageActor.h>
+#include <vtkTexturedSphereSource.h>
+#include <vtkXMLPolyDataReader.h>
 
-int main(int argc, char* argv[])
+//测试：../data/masonry.bmp ../data/cow.vtp
+int main (int argc, char *argv[])
 {
-    if(argc < 2)
+    if (argc < 3)
     {
-        std::cout<<argv[0]<<" "<<"ImageFile(*.bmp)"<<std::endl;
+        std::cout << "Usage: " << argv[0]
+        << "texture(.png)"
+            << "model(.vtp)" <<std::endl;
         return EXIT_FAILURE;
     }
 
-    vtkSmartPointer<vtkBMPReader> reader =
-            vtkSmartPointer<vtkBMPReader>::New();
-    reader->SetFileName(argv[1]);
-    reader->Update();
+    double translate[3];
+    translate[0] = 10.0;
+    translate[1] = 0.0;
+    translate[2] = 0.0;
+    std::cout << translate[0] << ", "
+        << translate[1] << ", "
+        << translate[2] << "\n";
 
-    vtkSmartPointer<vtkImageActor> redActor =
-            vtkSmartPointer<vtkImageActor>::New();
-    redActor->SetInputData(reader->GetOutput());
+    vtkSmartPointer<vtkBMPReader> texReader =
+        vtkSmartPointer<vtkBMPReader>::New();
+    texReader->SetFileName(argv[1]);
 
-    vtkSmartPointer<vtkRenderer> redRenderer =
-            vtkSmartPointer<vtkRenderer>::New();
-    redRenderer->AddActor(redActor);
-    redRenderer->SetBackground(1.0,1.0,1.0);
+    vtkSmartPointer<vtkTexture> texture =
+        vtkSmartPointer<vtkTexture>::New();
+    texture->SetInputConnection(texReader->GetOutputPort());
 
-    // Setup render window
+    vtkSmartPointer<vtkXMLPolyDataReader> modelReader =
+        vtkSmartPointer<vtkXMLPolyDataReader>::New();
+    modelReader->SetFileName(argv[2]);
+
+    vtkSmartPointer<vtkTextureMapToCylinder> texturemap =
+        vtkSmartPointer<vtkTextureMapToCylinder>::New();
+    texturemap->SetInputConnection(modelReader->GetOutputPort());
+
+    vtkSmartPointer<vtkPolyDataMapper> mapper =
+        vtkSmartPointer<vtkPolyDataMapper>::New();
+    mapper->SetInputConnection(texturemap->GetOutputPort());
+
+    vtkSmartPointer<vtkActor> actor =
+        vtkSmartPointer<vtkActor>::New();
+    actor->SetMapper( mapper );
+    actor->SetTexture( texture );
+
+    vtkSmartPointer<vtkRenderer> renderer =
+        vtkSmartPointer<vtkRenderer>::New();
+    renderer->AddActor(actor);
+    renderer->SetBackground(1.0, 1.0, 1.0);
+
     vtkSmartPointer<vtkRenderWindow> renderWindow =
         vtkSmartPointer<vtkRenderWindow>::New();
-    renderWindow->AddRenderer(redRenderer);
-    renderWindow->SetSize( 800, 600 );
-    renderWindow->Render();
-    renderWindow->SetWindowName("DisplayImageExample2");
+    renderWindow->AddRenderer( renderer );
 
-    // Setup render window interactor
-    vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+    vtkSmartPointer<vtkRenderWindowInteractor> renWinInteractor =
         vtkSmartPointer<vtkRenderWindowInteractor>::New();
-    vtkSmartPointer<vtkInteractorStyleImage> style =
-        vtkSmartPointer<vtkInteractorStyleImage>::New();
+    renWinInteractor->SetRenderWindow( renderWindow );
 
-    renderWindowInteractor->SetInteractorStyle(style);
-    // Render and start interaction
-    renderWindowInteractor->SetRenderWindow(renderWindow);
-    renderWindowInteractor->Initialize();
-    renderWindowInteractor->Start();
+    renderWindow->SetSize(640, 480);
+    renderWindow->Render();
+    renderWindow->SetWindowName("TextureMap");
+    renderWindow->Render();
+    renderWindow->Render();
+    renWinInteractor->Start();
 
     return EXIT_SUCCESS;
 }

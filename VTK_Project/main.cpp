5f8c4ca9c9daf9e4ce1e4d4da88e6a218f2da005
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-12-01 11:22:46 +0800

    test ok

--- VTK_Project/main.cpp
+++ VTK_Project/main.cpp
@@ -1,73 +1,93 @@
 ﻿/**********************************************************************
 
-  文件名: 2.1_RenderCylinder.cpp
+  文件名: 6.3_PolyDataLapLasianSmooth.cpp
   Copyright (c) 张晓东, 罗火灵. All rights reserved.
-  更多信息请访问: 
+  更多信息请访问:
     http://www.vtkchina.org (VTK中国)
-	http://blog.csdn.net/www_doling_net (东灵工作室) 
+    http://blog.csdn.net/www_doling_net (东灵工作室)
 
 **********************************************************************/
 
 #include <vtkSmartPointer.h>
+#include <vtkPolyDataReader.h>
+#include <vtkPolyData.h>
+#include <vtkSmoothPolyDataFilter.h>
+#include <vtkWindowedSincPolyDataFilter.h>
+#include <vtkSphereSource.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindowInteractor.h>
 
-#include <vtkImageData.h>
-#include <vtkImageCanvasSource2D.h>
-#include <vtkInteractorStyleImage.h>
-#include <vtkImageActor.h>
-
-int main(int argc, char* argv[])
+//测试文件：../data/fran_cut.vtk
+int main(int argc, char *argv[])
 {
-//    if(argc < 2)
-//    {
-//        std::cout<<argv[0]<<" "<<"TextureFile(*.jpg)"<<std::endl;
-//        return EXIT_FAILURE;
-//    }
+    if(argc < 2)
+    {
+        std::cout<<argv[0]<<" *.vtk"<<std::endl;
+        return EXIT_FAILURE;
+    }
 
-    vtkSmartPointer<vtkImageCanvasSource2D> canvas =
-            vtkSmartPointer<vtkImageCanvasSource2D>::New();
-    canvas->SetScalarTypeToUnsignedChar();
-    canvas->SetNumberOfScalarComponents(1);
-    canvas->SetExtent(0,100,0,100,0,0);
-    canvas->SetDrawColor(0,0,0,0);
-    canvas->FillBox(0,100,0,100);
-    canvas->SetDrawColor(255,0,0,0);
-    canvas->FillBox(20,40,20,40);
-    canvas->Update();
+    vtkSmartPointer<vtkPolyDataReader> reader =
+        vtkSmartPointer<vtkPolyDataReader>::New();
+    reader->SetFileName(argv[1]);
+    reader->Update();
 
-    vtkSmartPointer<vtkImageActor> redActor =
-            vtkSmartPointer<vtkImageActor>::New();
-    redActor->SetInputData(canvas->GetOutput());
+    vtkSmartPointer<vtkSmoothPolyDataFilter> smoothFilter =
+        vtkSmartPointer<vtkSmoothPolyDataFilter>::New();
+    smoothFilter->SetInputConnection(reader->GetOutputPort());
+    smoothFilter->SetNumberOfIterations(200);
+    smoothFilter->Update();
 
-    double redViewport[4] = {0.0, 0.0, 1.0, 1.0};
-    vtkSmartPointer<vtkRenderer> redRenderer =
-            vtkSmartPointer<vtkRenderer>::New();
-    redRenderer->SetViewport(redViewport);
-    redRenderer->AddActor(redActor);
-    redRenderer->ResetCamera();
-    redRenderer->SetBackground(1.0,1.0,1.0);
+    vtkSmartPointer<vtkPolyDataMapper> inputMapper =
+        vtkSmartPointer<vtkPolyDataMapper>::New();
+    inputMapper->SetInputConnection(reader->GetOutputPort());
+    vtkSmartPointer<vtkActor> inputActor =
+        vtkSmartPointer<vtkActor>::New();
+    inputActor->SetMapper(inputMapper);
 
-    // Setup render window
+    vtkSmartPointer<vtkPolyDataMapper> smoothedMapper =
+        vtkSmartPointer<vtkPolyDataMapper>::New();
+    smoothedMapper->SetInputConnection(smoothFilter->GetOutputPort());
+
+    vtkSmartPointer<vtkActor> smoothedActor =
+        vtkSmartPointer<vtkActor>::New();
+    smoothedActor->SetMapper(smoothedMapper);
+
+    double leftViewport[4] = {0.0, 0.0, 0.5, 1.0};
+    double rightViewport[4] = {0.5, 0.0, 1.0, 1.0};
+
+    vtkSmartPointer<vtkRenderer> leftRenderer =
+        vtkSmartPointer<vtkRenderer>::New();
+
+    leftRenderer->SetViewport(leftViewport);
+    leftRenderer->AddActor(inputActor);
+    leftRenderer->SetBackground(0.8, 0.8, 0.8);
+    leftRenderer->ResetCamera();
+
+    vtkSmartPointer<vtkRenderer> rightRenderer =
+        vtkSmartPointer<vtkRenderer>::New();
+    rightRenderer->SetViewport(rightViewport);
+    rightRenderer->AddActor(smoothedActor);
+    rightRenderer->SetBackground(0.8, 0.8, 0.8);
+    rightRenderer->SetActiveCamera(leftRenderer->GetActiveCamera());
+    rightRenderer->ResetCamera();
+
     vtkSmartPointer<vtkRenderWindow> renderWindow =
         vtkSmartPointer<vtkRenderWindow>::New();
-    renderWindow->AddRenderer(redRenderer);
-    renderWindow->SetSize( 800, 600 );
+    renderWindow->AddRenderer(leftRenderer);
+    renderWindow->AddRenderer(rightRenderer);
+    renderWindow->SetSize(640, 320);
     renderWindow->Render();
-    renderWindow->SetWindowName("ImageCanvasSource2D");
+    renderWindow->SetWindowName("PolyDataLapLasianSmooth");
 
-    // Setup render window interactor
-    vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+    vtkSmartPointer<vtkRenderWindowInteractor> interactor =
         vtkSmartPointer<vtkRenderWindowInteractor>::New();
-    vtkSmartPointer<vtkInteractorStyleImage> style =
-        vtkSmartPointer<vtkInteractorStyleImage>::New();
+    interactor->SetRenderWindow(renderWindow);
 
-    renderWindowInteractor->SetInteractorStyle(style);
-    // Render and start interaction
-    renderWindowInteractor->SetRenderWindow(renderWindow);
-    renderWindowInteractor->Initialize();
-    renderWindowInteractor->Start();
+    renderWindow->Render();
+    interactor->Start();
 
     return EXIT_SUCCESS;
 }
